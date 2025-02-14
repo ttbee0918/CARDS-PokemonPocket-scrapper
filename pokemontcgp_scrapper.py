fe4aed069b88a1112c871ceb2e84949f388cfee3
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-02-14 20:22:53 +0100

    Auto update info

--- pokemontcgp_scrapper.py
+++ pokemontcgp_scrapper.py
@@ -356,7 +356,7 @@ def convert_cards_to_json(start_id, end_id, filename):
 init_time = time.time()
 start_id = 1
 end_id = 286
-filename = "cards_data.json"
+filename = "pokemon_cards.json"
 convert_cards_to_json(start_id, end_id, filename)
 end_time = time.time()
 print(
