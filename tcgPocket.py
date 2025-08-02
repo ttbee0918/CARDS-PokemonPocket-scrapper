edb64081a984f9e372b3557ea31de85ae2ddd372
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-08-02 17:34:36 +0200

    Add TGCPocket class

---
+++ tcgPocket.py
@@ -0,0 +1,68 @@
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import requests
+import set
+
+
+class TGCPocket:
+
+    def __init__(self):
+        self.url = "https://pocket.limitlesstcg.com/cards"
+
+        response = requests.get(self.url)
+        response.raise_for_status()
+        self.soup = BeautifulSoup(response.content, "html.parser")
+
+        self.setAll()
+
+    def setAll(self) -> None:
+        """
+        Set all attributes.
+
+        Args:
+            - None
+
+        Returns:
+            - None
+        """
+        self.setSets()
+
+    def setSets(self) -> None:
+        """
+        Set the card sets.
+
+        Args:
+            - None
+
+        Returns:
+            - None
+        """
+        self.sets: list[set.Set] = []
+
+        parsedUrl = urlparse(self.url)
+        origin = f"{parsedUrl.scheme}://{parsedUrl.hostname}"
+
+        setsElement = self.soup.find("table", class_="data-table sets-table striped")
+
+        for row in setsElement.find_all("tr"):
+            link = row.find("a", href=True)
+
+            if link:
+                self.sets.append(set.Set(f"{origin}{link['href']}"))
+
+    def getCardData(self) -> list[dict]:
+        """
+        Get card data from all sets.
+
+        Args:
+            - None
+
+        Returns:
+            - list[dict]: List of dictionaries containing card data
+        """
+        cardData = []
+
+        for setInstance in self.sets:
+            cardData.extend(setInstance.getCardData())
+
+        return cardData
